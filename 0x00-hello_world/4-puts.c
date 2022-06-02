c8256196f07498d80e334fea06ab288f0c3d7626
kenateg <kyriosbee@gmail.com>
2022-06-02 03:59:51 -0700

    changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,6 @@
 /*
  * My first code for c bruv!
 */
-int main(void)  //description for main
+int main(void)/*description for main*/
 { puts("\"Programming is like building a multilingual puzzle");
 return (0); }

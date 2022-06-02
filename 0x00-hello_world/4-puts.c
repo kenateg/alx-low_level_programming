1e9b81296882b248d1578542209cb96c06ed7414
kenateg <kyriosbee@gmail.com>
2022-06-02 04:47:20 -0700

    changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main - displays output 
  * Description: describes the function below
  */
-int main (void) 
+int main(void) 
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0); }

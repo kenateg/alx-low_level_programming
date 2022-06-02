6fa0918226b8d4c547008910b5c0ebcc4970690c
kenateg <kyriosbee@gmail.com>
2022-06-02 06:14:24 -0700

    changes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,6 @@
  * Return: prints out 1
  */
 int main(void)
-{ write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+{ write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19", 61);
 return (1);
 }

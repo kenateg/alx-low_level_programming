cccd54add0a19be11fb280af7dc325dbf2081c33
kenateg <kyriosbee@gmail.com>
2022-06-02 03:29:32 -0700

    changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,5 @@
 #include <stdio.h>
-#include <string.h>
-/*
- *
- * My first c command
- */
-int puts(const char *str)
 
 int main(void){
-
-	char str[250];
-
-	strcpy(str,"Programming is like building a multilingual puzzle");
-
-	puts(str);
-
+puts("Programming is like building a multilingual puzzle\n");
 }

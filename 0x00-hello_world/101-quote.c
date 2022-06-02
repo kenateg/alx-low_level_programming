a527a9c7f410e53d93e58de0af69a1c7af03af28
kenateg <kyriosbee@gmail.com>
2022-06-02 09:53:04 -0700

    changes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,10 +5,7 @@
  * Return: prints out 1
  */
 int main(void)
-{ FILE * fp;
-char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-fp = fopen("file.txt", "w");
-fwrite(str, 2, sizeof(str), fp);
-fclose(fp);
-return (1);
-}
+{
+char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, str1, 59);
+return (1); }

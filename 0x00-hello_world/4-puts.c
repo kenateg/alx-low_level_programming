f9b34133268eb23bd34c314da99804c45d6fc7fa
kenateg <kyriosbee@gmail.com>
2022-06-02 04:48:16 -0700

    changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
-/*
- * main - displays output 
+/** main - displays output 
  * Description: describes the function below
  */
 int main(void) 
-{
-puts("\"Programming is like building a multilingual puzzle");
+{puts("\"Programming is like building a multilingual puzzle");
 return (0); }

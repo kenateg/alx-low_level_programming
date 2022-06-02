a4d6762a1e9bc0c3905ae4e6093b0efbf1c74f63
kenateg <kyriosbee@gmail.com>
2022-06-02 06:00:48 -0700

    changes

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - writes out the values
+ * Description: avails the tru values out.
+ * Return: prints out 1
+ */
+int main(void)
+{ write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+return (1);
+}

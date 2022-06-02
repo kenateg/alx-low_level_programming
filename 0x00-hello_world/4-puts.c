21bc7b774d2755bbc35774c27889c86a690f0a21
kenateg <kyriosbee@gmail.com>
2022-06-02 03:25:07 -0700

    changes

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+/*
+ *
+ * My first c command
+ */
+int main(void){
+
+	char str[250];
+
+	strcpy(str,"Programming is like building a multilingual puzzle");
+
+	puts(str);
+
+}

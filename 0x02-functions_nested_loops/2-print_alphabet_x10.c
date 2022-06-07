1628d023d3f1821140ae57439ce1a37c2b6c1401
kenateg <kyriosbee@gmail.com>
2022-06-07 06:29:05 -0700

    wrote the x10task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabets ten times
+ * Return: returns null
+ */
+void print_alphabet_x10(void)
+{
+	int x;
+	char v;
+
+	for (x = 0; x < 10; x++)
+		for (v = 'a'; v <= 'z'; v++)
+			_putchar(v);
+	_putchar('\n');

897da63e098d92fe5f73a66367c1a50a3e36eedf
kenateg <kyriosbee@gmail.com>
2022-06-07 05:33:34 -0700

    changed the whole value of the code _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - function prints out the value _putchar
  * Return: will return the value zero
  */
 int main(void)
 {
-	char character = '_putchar';
+	char character[8]  = "_putchar";
+	int x;
 
-	putchar(character);
-	retur (0);
+	for (x = 0; x <= 7; x++)
+	       _putchar(character[x]);
+	_putchar('\n');
+	
+	return (0);
 }

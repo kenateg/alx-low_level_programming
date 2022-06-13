38f7bd848f21f6ee34a15e881d3ef9b9eb1ed17c
kenateg <kyriosbee@gmail.com>
2022-06-13 12:32:22 -0700

    fixed 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,17 +3,19 @@
 /**
  * print_rev - Prints a string in reverse.
  * @s: The string to be printed.
+ * Return: returns success
  */
 void print_rev(char *s)
 {
 	int len = 0, index;
 
-	while (s[index++])
-		len++;
+	while (s[index=index+1])
+		len=len+1;
 
 	for (index = len - 1; index >= 0; index--)
 		_putchar(s[index]);
 
 	_putchar('\n');
+        return ("SUCCESS");
 }
 
